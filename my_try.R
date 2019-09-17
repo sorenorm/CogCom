4d1d827816b841cdf9fa8278d76e99be5e284119
sorenorm <sorenorm@live.dk>
2019-09-17 12:49:40 +0200

    Goody

---
+++ my_try.R
@@ -0,0 +1,11 @@
+?list.files()
+
+getwd()
+
+sarah_data <- list.files(path = "/CogSci/CogCom/Week 3/Class/CogCom/data", pattern = NULL, all.files = FALSE, full.names = FALSE, recursive = FALSE, ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
+
+dir(path = "/CogSci/CogCom/Week 3/Class/CogCom/data", pattern = NULL, all.files = FALSE, full.names = FALSE, recursive = FALSE, ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
+
+list.dirs(path = "/CogSci/CogCom/Week 3/Class/CogCom/data", full.names = TRUE, recursive = TRUE)
+
+
